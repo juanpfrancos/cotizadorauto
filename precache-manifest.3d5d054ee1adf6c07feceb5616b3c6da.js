b6ac3cbb92a762b0f932290f06e271a77f5eb2a3
juanpfrancos <juanpanime@gmail.com>
2020-01-17 20:44:32 -0500

    Updates

---
+++ precache-manifest.3d5d054ee1adf6c07feceb5616b3c6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a06fcb2cc0a7c3e11d64c8dedee8b3",
+    "url": "/cotizadorauto/index.html"
+  },
+  {
+    "revision": "5d751ae6180df4b91d2e",
+    "url": "/cotizadorauto/static/css/main.ea55a18a.chunk.css"
+  },
+  {
+    "revision": "d5511061c0bd151c6d86",
+    "url": "/cotizadorauto/static/js/2.ce77651b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cotizadorauto/static/js/2.ce77651b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d751ae6180df4b91d2e",
+    "url": "/cotizadorauto/static/js/main.40004b6d.chunk.js"
+  },
+  {
+    "revision": "2b38d862b87609ca4133",
+    "url": "/cotizadorauto/static/js/runtime-main.d35ed88d.js"
+  }
+]);
